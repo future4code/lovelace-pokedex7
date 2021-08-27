5de07e68dbf4846f50053f20171c020e9729ea47
MatheusOliveiraSantos <matheuso.santos@hotmail.com>
2021-08-27 14:14:02 -0300

    Details compretasso

--- pokedex/src/pages/PokemonDetail/PokemonDetail.js
+++ pokedex/src/pages/PokemonDetail/PokemonDetail.js
@@ -1,8 +1,45 @@
-import React from 'react'
-import { PokemonHeader } from '../../components/Header'
+import React from "react";
+import { PokemonHeader } from "../../components/Header";
+import { GlobalContext } from "../../components/GlobalStorage";
+import axios from "axios";
 
 export default function PokemonDetail() {
-    return (
-        <PokemonHeader/>
-    )
+  const [pokemonDetails, setPokemonDetails] = React.useState([]);
+
+  const global = React.useContext(GlobalContext);
+
+  const getPokemonDetails = async () => {
+    let response;
+    try {
+      response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${global.pokemonName}`
+      );
+    } catch (error) {
+      response = null;
+    } finally {
+      setPokemonDetails([response.data]);
+    }
+}; 
+
+    const renderPokemonDetail = () => {
+        const detailsMap = pokemonDetails.map((detail) => {
+            return (
+                <div>
+                    {detail.name}
+                </div>
+            )
+        })  
+        return detailsMap
+    }
+
+React.useEffect(() => {
+    getPokemonDetails();
+}, []);
+
+  return (
+    <>
+    <PokemonHeader /> 
+    {renderPokemonDetail()}
+    </>
+  );
 }
